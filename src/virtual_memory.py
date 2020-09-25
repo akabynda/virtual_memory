427a74a7f9a9c926cf6831f742ddc559cfdfb3ec
st084331 <st084331@student.spbu.ru>
2020-09-26 00:07:54 +0600

    version 0.1.4

--- src/virtual_memory.py
+++ src/virtual_memory.py
@@ -5,72 +5,76 @@ def parser_args():
     p.add_argument('--file', type=str)
     p.add_argument('--page', type=str)
     p.add_argument('--time', type=str)
+    p.add_argument('--algo', type=str)
     return p
 
 
-def making_log(file):  #преобразуем прочитанный файл
+def making_log(file):  # преобразуем прочитанный файл
     log = file.read().replace(' ', '').replace('#', '').replace(':', '').replace('\n', ';') + ';'
-    a = [] #номера кадра
-    b = [] #время манипуляции
-    c = [] #тип манипуляции
-    g = [] #страницы
-    while (log!= ''):
-        f1 = log.index(',')
-        n = log[:f1]
-        a.append(n)
-        t = log[(f1 + 1):(f1 + 5)]
-        b.append(t)
-        v = log[(f1 + 6):(f1 + 7)]
-        c.append(v)
-        f2 = log.index(';')
-        d = log[(f1 + 8):f2]
-        g.append(d)
-        log = log[(f2 + 1):]
-    log = [a,b,c,g] #
+    frames = [] # номера кадра
+    times = [] # время манипуляции
+    manipulations = [] # тип манипуляции
+    pages = [] # страницы
+    while (log!= ''): # трансформируем строку log в список
+        f1 = log.index(',') # первая запятая
+        N = log[:f1] # номер кадра
+        frames.append(N) # добавляем номер кадра
+        T = log[(f1 + 1):(f1 + 5)] # время
+        times.append(T) # добавляем время
+        M = log[(f1 + 6):(f1 + 7)] # манипуляция
+        manipulations.append(M) # добавляем манипуляцию
+        f2 = log.index(';') # конец операции
+        P = log[(f1 + 8):f2] # страница
+        pages.append(P) # добавляем страницу
+        log = log[(f2 + 1):] # обновляем лог
+    log = [frames,times,manipulations,pages] # готовый список списков
     return log
 
 def LRU_log(log):  # преобразуем прочитанный файл
     return log
 
-def FIFO_log(log):  #преобразуем прочитанный файл
-    return log
+def FIFO_log(log):  # преобразуем прочитанный файл под FIFO
+    times = log[1]
+    frames = log[0]
+    pages = log[3]
+    FIFO_t = []
+    FIFO_p = []
+    for i in range(int(max(frames))):
+        FIFO_t.append(0)
+        FIFO_p.append(0)
+    for i in range(len(frames)):
+        j = int(frames[i]) - 1
+        FIFO_t[j] = int(times[i])
+        FIFO_p[j] = int(pages[i])
+    FIFO_log = [FIFO_t, FIFO_p]
+    return FIFO_log
+
+def FIFO_algorithm(FIFO_log, page, time):
+    FIFO_p = FIFO_log[1]
+    FIFO_t = FIFO_log[0]
+    FIFO_bul = True  # проверяется есть ли данная страница в оперативной памяти
+    for i in range(len(FIFO_p)):
+        if (FIFO_p[i] != page):
+            FIFO_bul = False
+    if (FIFO_bul == False):
+        temp = FIFO_t[0]
+        k = 0
+        for i in range(len(FIFO_t)):
+            if (FIFO_t[i] < temp):
+                temp = FIFO_t[i]
+                k = i
+        FIFO_t[k] = int(time)
+        FIFO_p[k] = int(page)
+    FIFO_log = [FIFO_t, FIFO_p]
+    return FIFO_log
+
 
 parser = parser_args()
 file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', parser.parse_args().file + '.txt'), encoding="utf - 8")
 page = parser.parse_args().page
 time = parser.parse_args().time.replace(':', '')
-old_log = making_log(file)
-FIFO_log = []
-print(old_log)
-b = old_log[1]
-a = old_log[0]
-g = old_log[3]
-c = old_log[2]
-print(a)
-print(b)
-FIFO = []
-FIFO_e = []
-for i in range(int(max(a))):
-    FIFO.append(0)
-    FIFO_e.append(0)
-for i in range(len(a)):
-    j = int(a[i]) - 1
-    FIFO[j] = int(b[i])
-    FIFO_e[j] = int(g[i])
-print(FIFO)
-print(FIFO_e)
-FIFO_bul = True #проверяется есть ли данная страница в оперативной памяти
-for i in range(len(FIFO_e)):
-    if(FIFO_e[i] != page):
-        FIFO_bul = False
-if(FIFO_bul == False):
-    temp = FIFO[0]
-    k = 0
-    for i in range(len(FIFO)):
-        if(FIFO[i] < temp):
-            temp = FIFO[i]
-            k = i
-    FIFO[k] = int(time)
-    FIFO_e[k] = int(page)
-print(FIFO)
-print(FIFO_e)
+algorithm = parser.parse_args().algo
+original_log = making_log(file)
+
+FIFO_log = FIFO_algorithm(FIFO_log(original_log), page, time)
+print(FIFO_log)
