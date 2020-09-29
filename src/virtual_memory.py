96d9233477051bbffe7562c5847fdafaa6b38974
st084331 <st084331@student.spbu.ru>
2020-09-29 13:11:18 +0300

    version 1.0

--- src/virtual_memory.py
+++ src/virtual_memory.py
@@ -3,14 +3,6 @@ import os
 import time
 from datetime import datetime
 
-def parser_args():
-    p = argparse.ArgumentParser()
-    p.add_argument('--file', type=str)
-    p.add_argument('--page', type=str)
-    p.add_argument('--time', type=str)
-    p.add_argument('--algo', type=str)
-    return p
-
 class Memory:
 
     def formating_time(self, time):
@@ -65,7 +57,6 @@ class Memory:
             j = int(frames[i]) - 1
             OPT_t[j] = OPT_t_max - OPT_t[j]
         OPT_log = [OPT_t, OPT_p]
-        print(OPT_log)
         return OPT_log
 
     def FIFO_log(self, log):  # преобразуем прочитанный файл под FIFO
@@ -104,128 +95,144 @@ class Memory:
         LRU_log = [LRU_t, LRU_p, LRU_m] # составляем целостный log
         return LRU_log
 
-    def OPT_algorithm(self, OPT_log, page, time, file):
-        file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', parser.parse_args().file + '.txt'), 'a')
-        OPT_t = OPT_log[0]
-        OPT_p = OPT_log[1]
-        OPT_bul = 0
-        k = 0
-        temp = OPT_t[0]
-        for i in range(len(OPT_t)): # ищем самое раннее обращение
-            if(OPT_t[i] > temp):
-                k = i
-                temp = OPT_t[i]
-        for i in range(len(OPT_p)): # проверяем наличие страницы
-            if (OPT_p[i] == int(page)):
-                OPT_bul += 1
-        if (OPT_bul == 0):  # счетчик OPT
-            global OPT_count
-            OPT_count += 1
-        global algorithm
-        if (algorithm == 'OPT'):
-            if (OPT_bul == 0): # если страницы нет
-                new_str = '/#' + str(k+1) + ' , ' + Memory().formating_time(time) + ' , w , ' + str(page)
-            else:
-                new_str ='/#' + str(k+1) + ' , ' + Memory().formating_time(time) + ' , r , ' + str(page)
-            file.write(new_str.replace('/', '\n'))
-        OPT_p[k] = int(page)
-        for i in range (len(OPT_t)): # скорость работы не позволяет времени измениться, поэтому прибавляем
-                                     # любую константу
-            OPT_t[i] += 10
-        OPT_t[k] = 0
-        OPT_log = [OPT_t, OPT_p]
-        return OPT_log
+    def OPT_algorithm(self, OPT_log, pages, time, name_file, algo):
+        OPT_count = 0
+        while pages != '':
+            f1 = pages.index(';')
+            page = pages[:f1]  # берем одну страницу
+            pages = pages[(f1 + 1):]  # удаляем ее из списка
+            file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file), 'a')
+            OPT_t = OPT_log[0]
+            OPT_p = OPT_log[1]
+            OPT_bul = 0
+            k = 0
+            temp = OPT_t[0]
+            for i in range(len(OPT_t)): # ищем самое раннее обращение
+                if(OPT_t[i] > temp):
+                    k = i
+                    temp = OPT_t[i]
+            for i in range(len(OPT_p)): # проверяем наличие страницы
+                if (OPT_p[i] == int(page)):
+                    OPT_bul += 1
 
-    def LRU_algorithm(self, LRU_log, page, time, file):
-        file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', parser.parse_args().file + '.txt'), 'a')
-        LRU_p = LRU_log[1]
-        LRU_t = LRU_log[0]
-        LRU_m = LRU_log[2]
-        LRU_bul = 0  # проверяется есть ли данная страница в оперативной памяти
-        temp = LRU_t[0]
-        k = 0
-        for i in range(len(LRU_t)):  # ищем самое раннее обращение
-            if (LRU_t[i] < temp and LRU_m[i] == 'r'):
-                temp = LRU_t[i]
-                k = i
-        if(k == 0 and LRU_m[0] == 'w'):
+            if (OPT_bul == 0):  # счетчик OPT
+                OPT_count += 1
+            if (algo == 'OPT'):
+                if (OPT_bul == 0): # если страницы нет
+                    new_str = '/#' + str(k+1) + ' , ' + Memory().formating_time(time) + ' , w , ' + str(page)
+                else:
+                    new_str ='/#' + str(k+1) + ' , ' + Memory().formating_time(time) + ' , r , ' + str(page)
+                file.write(new_str.replace('/', '\n'))
+            OPT_p[k] = int(page)
+            for i in range (len(OPT_t)): # скорость работы не позволяет времени измениться, поэтому прибавляем
+                                         # любую константу
+                OPT_t[i] += 10
+            OPT_t[k] = 0
+            OPT_log = [OPT_t, OPT_p]
+            return OPT_log
+        print('OPT counter:', OPT_count)
+
+    def LRU_algorithm(self, LRU_log, pages, time, name_file, algo):
+        LRU_count = 0
+        while pages != '':
+            f1 = pages.index(';')
+            page = pages[:f1]  # берем одну страницу
+            pages = pages[(f1 + 1):]  # удаляем ее из списка
+            file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file), 'a')
+            LRU_p = LRU_log[1]
+            LRU_t = LRU_log[0]
+            LRU_m = LRU_log[2]
+            LRU_bul = 0  # проверяется есть ли данная страница в оперативной памяти
+            temp = LRU_t[0]
+            k = 0
             for i in range(len(LRU_t)):  # ищем самое раннее обращение
-                if (LRU_t[i] < temp):
+                if (LRU_t[i] < temp and LRU_m[i] == 'r'):
                     temp = LRU_t[i]
                     k = i
-        for i in range(len(LRU_p)): # проверяем наличие страницы
-            if (LRU_p[i] == int(page)):
-                LRU_bul += 1
-        LRU_t[k] = time
-        LRU_p[k] = int(page)
-        if (LRU_bul == 0):  # счетчик LRU
-            global LRU_count
-            LRU_count += 1
-            LRU_m[k] = 'w'
-        else:
-            LRU_m[k] = 'r'
-        global algorithm
-        if(algorithm == 'LRU'):
-            if (LRU_bul == 0): # если страници нет
-                new_str = '/#' + str(k+1) + ' , ' + Memory().formating_time(LRU_t[k]) + ' , w , ' + str(LRU_p[k])
+            if(k == 0 and LRU_m[0] == 'w'):
+                for i in range(len(LRU_t)):  # ищем самое раннее обращение
+                    if (LRU_t[i] < temp):
+                        temp = LRU_t[i]
+                        k = i
+            for i in range(len(LRU_p)): # проверяем наличие страницы
+                if (LRU_p[i] == int(page)):
+                    LRU_bul += 1
+            LRU_t[k] = time
+            LRU_p[k] = int(page)
+            if (LRU_bul == 0):  # счетчик LRU
+                LRU_count += 1
+                LRU_m[k] = 'w'
             else:
-                new_str ='/#' + str(k+1) + ' , ' + Memory().formating_time(LRU_t[k]) + ' , r , ' + str(LRU_p[k])
-            file.write(new_str.replace('/', '\n'))
-        LRU_log = [LRU_t, LRU_p, LRU_m]
-        file.close()
-        return LRU_log # обновляем log
+                LRU_m[k] = 'r'
+            if(algo == 'LRU'):
+                if (LRU_bul == 0): # если страници нет
+                    new_str = '/#' + str(k+1) + ' , ' + Memory().formating_time(LRU_t[k]) + ' , w , ' + str(LRU_p[k])
+                else:
+                    new_str ='/#' + str(k+1) + ' , ' + Memory().formating_time(LRU_t[k]) + ' , r , ' + str(LRU_p[k])
+                file.write(new_str.replace('/', '\n'))
+            LRU_log = [LRU_t, LRU_p, LRU_m]
+            file.close()
+            return LRU_log # обновляем log
+        print('LRU counter:', LRU_count)
 
-    def FIFO_algorithm(self, FIFO_log, page, time, file):
-        file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', parser.parse_args().file + '.txt'), 'a')
-        FIFO_p = FIFO_log[1]
-        FIFO_t = FIFO_log[0]
-        FIFO_bul = 0  # проверяется есть ли данная страница в оперативной памяти
-        temp = FIFO_t[0]
-        k = 0
-        for i in range(len(FIFO_t)):  # находим самое раннее обращение
-            if (FIFO_t[i] < temp):
-                temp = FIFO_t[i]
-                k = i
-        FIFO_t[k] = time
-        for i in range(len(FIFO_p)): # проверяем наличие страницы
-            if (FIFO_p[i] == int(page)):
-                FIFO_bul += 1
-        FIFO_p[k] = int(page)
-        if (FIFO_bul == 0): # счетчик FIFO
-            global FIFO_count
-            FIFO_count += 1
-        global algorithm
-        if(algorithm == 'FIFO'):
-            if (FIFO_bul == 0): # если страницы нет
-                new_str = '/#' + str(k+1) + ' , ' + Memory().formating_time(FIFO_t[k]) + ' , w , ' + str(FIFO_p[k])
-            else:
-                new_str ='/#' + str(k+1) + ' , ' + Memory().formating_time(FIFO_t[k]) + ' , r , ' + str(FIFO_p[k])
-            file.write(new_str.replace('/', '\n'))
-        FIFO_log = [FIFO_t, FIFO_p]
-        file.close()
-        return FIFO_log # обновляем log
+    def FIFO_algorithm(self, FIFO_log, pages, time, name_file, algo):
+        FIFO_count = 0
+        while pages != '':
+            f1 = pages.index(';')
+            page = pages[:f1]  # берем одну страницу
+            pages = pages[(f1 + 1):]  # удаляем ее из списка
+            file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file), 'a')
+            FIFO_p = FIFO_log[1]
+            FIFO_t = FIFO_log[0]
+            FIFO_bul = 0  # проверяется есть ли данная страница в оперативной памяти
+            temp = FIFO_t[0]
+            k = 0
+            for i in range(len(FIFO_t)):  # находим самое раннее обращение
+                if (FIFO_t[i] < temp):
+                    temp = FIFO_t[i]
+                    k = i
+            FIFO_t[k] = time
+            for i in range(len(FIFO_p)): # проверяем наличие страницы
+                if (FIFO_p[i] == int(page)):
+                    FIFO_bul += 1
+            FIFO_p[k] = int(page)
+            if (FIFO_bul == 0): # счетчик FIFO
+                FIFO_count += 1
+            if(algo== 'FIFO'):
+                if (FIFO_bul == 0): # если страницы нет
+                    new_str = '/#' + str(k+1) + ' , ' + Memory().formating_time(FIFO_t[k]) + ' , w , ' + str(FIFO_p[k])
+                else:
+                    new_str ='/#' + str(k+1) + ' , ' + Memory().formating_time(FIFO_t[k]) + ' , r , ' + str(FIFO_p[k])
+                file.write(new_str.replace('/', '\n'))
+            FIFO_log = [FIFO_t, FIFO_p]
+            file.close()
+            return FIFO_log # обновляем log
+        print('FIFO counter:', FIFO_count)
+
+def parser_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('--file', type=str)
+    p.add_argument('--page', type=str)
+    p.add_argument('--time', type=str)
+    p.add_argument('--algo', type=str)
+    return p
 
 if __name__ == '__main__':
     memory1 = Memory()
     parser = parser_args()
-    file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', parser.parse_args().file + '.txt'),
+    name_file = parser.parse_args().file + '.txt'
+    file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file),
                 encoding="utf - 8")
     pages = parser.parse_args().page + ';' # ввод страниц
+    algorithm = 'NO'
     algorithm = parser.parse_args().algo # выбираем алгоритм, который будет работать с файлом
-    print(file)
     original_log = memory1.making_log(file) # создаем оригинальный log
     file.close()
-    OPT_count = LRU_count = FIFO_count = 0 # счетчик для теста
     LRU_log = memory1.LRU_log(original_log)
     FIFO_log = memory1.FIFO_log(original_log)
     OPT_log = memory1.OPT_log(original_log)
-    while pages != '':
-        f1 = pages.index(';')
-        page = pages[:f1] # берем одну страницу
-        pages = pages[(f1 + 1):] # удаляем ее из списка
-        OPT_log = memory1.OPT_algorithm(OPT_log, page, time.time() + 60, file) # скорость работы привышает скорость
+    OPT_log = memory1.OPT_algorithm(OPT_log, pages, time.time() + 60, name_file, algorithm) # скорость работы привышает скорость
                                                                        # обновления времени, поэтому прибавляем
                                                                        # любую константу
-        LRU_log = memory1.LRU_algorithm(LRU_log, page, time.time() + 60, file)
-        FIFO_log = memory1.FIFO_algorithm(FIFO_log, page, time.time() + 60, file)
-    print('OPT:', OPT_count, 'FIFO:', FIFO_count, 'LRU:', LRU_count)
+    LRU_log = memory1.LRU_algorithm(LRU_log, pages, time.time() + 60, name_file, algorithm)
+    FIFO_log = memory1.FIFO_algorithm(FIFO_log, pages, time.time() + 60, name_file, algorithm)
--- src/virtual_memory_test.py
+++ src/virtual_memory_test.py
@@ -5,20 +5,39 @@ import os
 import datetime
 from virtual_memory import Memory
 
-file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data',
-                                 'log' + str(random.randint(1,4)) + '.txt'),
-                    encoding="utf - 8")
-print(file)
-
 class TestMemory(unittest.TestCase):
     print("Start of the test", datetime.datetime.now())
     memory = Memory()
 
-    def test_making_log(self):
-        print('Result of testing of making log:', )
+    def test_working_with_logs(self):
+        name_file = 'log' + str(random.randint(1, 4)) + '.txt'
+        file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file), encoding="utf - 8")
+        original_log = self.memory.making_log(file)
+        print('Result of testing of making FIFO log:', self.memory.FIFO_log(original_log))
+        print('Result of testing of making LRU log:', self.memory.LRU_log(original_log))
+        print('Result of testing of making OPT log:', self.memory.OPT_log(original_log))
+        file.close()
 
-    def test_FIFO_log(self):
-        print('Result of testing of making FIFO log:', self.memory.FIFO_log(self.memory.making_log(file)))
+    def test_for_algorithms(self):
+        name_file = 'log' + str(random.randint(1, 4)) + '.txt'
+        file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file), encoding="utf - 8")
+        print(file)
+        original_log = self.memory.making_log(file)
+        LRU_log = self.memory.LRU_log(original_log)
+        FIFO_log = self.memory.FIFO_log(original_log)
+        OPT_log = self.memory.OPT_log(original_log)
+        OPT_count = LRU_count = FIFO_count = 0
+        algo = 'NO'
+        pages = []
+        for i in range(50):                          # создаем случайный набор страниц
+            pages.append(str(random.randint(1,12)))
+        pages_str = ''
+        for i in range(50):
+            pages_str += (str(pages[i]) + ';')
+        print('Result of FIFO algorithm:', self.memory.FIFO_algorithm(FIFO_log, pages_str, time.time() + 60, name_file, algo))
+        print('Result of LRU algorithm:', self.memory.LRU_algorithm(LRU_log, pages_str, time.time() + 60, name_file, algo))
+        print('Result of OPT algorithm:', self.memory.OPT_algorithm(OPT_log, pages_str, time.time() + 60, name_file, algo))
+        file.close()
 
     print("End of the test", datetime.datetime.now())
 
